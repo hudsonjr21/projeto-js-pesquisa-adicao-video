18ddbc6cc168d2009e7d9023bbf7dfcd7c08e047
Hudson de Jesus Ferreira Junior <hudson.junior@MATRIZ.GRUPOADRIANOCOBUCCIO.LOCAL>
2023-05-24 11:21:10 -0300

    Parte 5 e final - Finalizado

--- js/mostrarVideos.js
+++ js/mostrarVideos.js
@@ -9,19 +9,23 @@ export default function constroiCard(titulo, descricao, url, imagem) {
     title="${titulo}" frameborder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
     allowfullscreen></iframe>
-    <div class="descricao-video">
-        <img src="${imagem}" alt="logo canal alura">
-        <h3>${titulo}</h3>
-        <p>${descricao}</p>
-    </div>`
+<div class="descricao-video">
+    <img src="${imagem}" alt="logo canal alura">
+    <h3>${titulo}</h3>
+    <p>${descricao}</p>
+</div>`
 
     return video;
 }
 
 async function listaVideos() {
-    const listaApi = await conectaApi.listaVideos();
-    listaApi.forEach(elemento => lista.appendChild(
-        constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
+    try {
+        const listaApi = await conectaApi.listaVideos();
+        listaApi.forEach(elemento => lista.appendChild(
+            constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
+    } catch {
+        lista.innerHTML = `<h2 class="mensagem__titulo">Não foi possível carregar a lista de vídeos</h2>`
+    }
 }
 
 listaVideos();
